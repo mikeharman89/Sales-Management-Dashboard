51061514bc7b56527f50c7055113a2776a817855
Michael Harman <mikeharman89@gmail.com>
2023-02-21 16:03:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const url = "https://public.tableau.com/views/TableauSampleData_16770192616910/S
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 750,
+    height: 700,
     width: 1800,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
